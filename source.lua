e4b70fc327c3818dd99e980c0f72ab38b710cad0
ArtixSoftware <75815567+ArtixSoftware@users.noreply.github.com>
2021-01-24 13:28:06 -0600

    pre_beta 0.1C

--- source.lua
+++ source.lua
@@ -1,32 +1,32 @@
-local Visuals = {}
+local module = {}
 
 local function Set(t, i, v)
 	t[i] = v;
 end
 
-function Visuals:BoxUpdate(Instance, Properties)
+function module:BoxUpdate(Instance, Properties)
 
 end
-function Visuals:BoxNew(StartCF, Size, Properties)
-    local NewBox = Drawing.new("Quad")
+function module:BoxNew(StartCF, Size, Properties)
+	local NewBox = Drawing.new("Quad")
 
-    local TopLeft, TLVis = Camera:WorldToViewportPoint((StartCF * CFrame.new(Size.X, Size.Y, 0)).Position)
-    local TopRight, TRVis = Camera:WorldToViewportPoint((StartCF * CFrame.new(-Size.X, Size.Y, 0)).Position)
-    local BottomRight, BRVis = Camera:WorldToViewportPoint((StartCF * CFrame.new(-Size.X, -Size.Y, 0)).Position)
-    local BottomLeft, BLVis = Camera:WorldToViewportPoint((StartCF * CFrame.new(Size.X, -Size.Y, 0)).Position)
+	local TopLeft, TLVis = Camera:WorldToViewportPoint((StartCF * CFrame.new(Size.X, Size.Y, 0)).Position)
+	local TopRight, TRVis = Camera:WorldToViewportPoint((StartCF * CFrame.new(-Size.X, Size.Y, 0)).Position)
+	local BottomRight, BRVis = Camera:WorldToViewportPoint((StartCF * CFrame.new(-Size.X, -Size.Y, 0)).Position)
+	local BottomLeft, BLVis = Camera:WorldToViewportPoint((StartCF * CFrame.new(Size.X, -Size.Y, 0)).Position)
 
-    if TLVis and TRVis and BRVis and BLVis then
-        Properties.PointA = Vector2.new(TopLeft.X, TopLeft.Y)
-        Properties.PointB = Vector2.new(TopRight.X, TopRight.Y)
-        Properties.PointC = Vector2.new(BottomRight.X, BottomRight.Y)
-        Properties.PointD = Vector2.new(BottomLeft.X, BottomLeft.Y)
-        for i,v in pairs(Properties) do
-            pcall(Set, NewBox, i, v)
-        end
-        return NewBox
-    else
-        NewBox:Remove()
-    end
+	if TLVis and TRVis and BRVis and BLVis then
+		Properties.PointA = Vector2.new(TopLeft.X, TopLeft.Y)
+		Properties.PointB = Vector2.new(TopRight.X, TopRight.Y)
+		Properties.PointC = Vector2.new(BottomRight.X, BottomRight.Y)
+		Properties.PointD = Vector2.new(BottomLeft.X, BottomLeft.Y)
+		for i,v in pairs(Properties) do
+			pcall(Set, NewBox, i, v)
+		end
+		return NewBox
+	else
+		NewBox:Remove()
+	end
 end
 
-return Visuals
+return module
