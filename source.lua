1e8684348d56e29404bcdef49e6b7c39ffbcff52
ArtixSoftware <75815567+ArtixSoftware@users.noreply.github.com>
2021-01-24 15:51:52 -0600

    Update source.lua

--- source.lua
+++ source.lua
@@ -39,7 +39,6 @@ function module:BoxNew(StartCF, Size, Properties)
 	local BottomLeft, BLVis = Camera:WorldToViewportPoint((StartCF * CFrame.new(Size.X, -Size.Y, 0)).Position)
 
     if TLVis and TRVis and BRVis and BLVis then
-        Properties.Visible = true
 		Properties.PointA = Vector2.new(TopLeft.X, TopLeft.Y)
 		Properties.PointB = Vector2.new(TopRight.X, TopRight.Y)
 		Properties.PointC = Vector2.new(BottomRight.X, BottomRight.Y)
@@ -52,6 +51,40 @@ function module:BoxNew(StartCF, Size, Properties)
         NewBox:Remove()
         return nil
 	end
+end
+
+function module:TextUpdate(Instance, Position, Properties, PositionOffset)
+    local Camera = game:GetService("Workspace").CurrentCamera
+    local ScreenPos, IsVisible = Camera:WorldToViewportPoint(Position)
+
+    if IsVisible then
+        PositionOffset = PositionOffset or Vector2.new(0,0)
+        Properties.Position = Vector2.new(ScreenPos.X + PositionOffset.X, ScreenPos.Y + PositionOffset.Y)
+        for i,v in pairs(Properties) do
+			pcall(Set, Instance, i, v)
+		end
+		return Instance
+    else
+        Instance:Remove()
+        return nil
+    end
+end
+function module:TextNew(Position, Properties, PositionOffset)
+    local Camera = game:GetService("Workspace").CurrentCamera
+    local NewText = Drawing.new("Text")
+    local ScreenPos, IsVisible = Camera:WorldToViewportPoint(Position)
+
+    if IsVisible then
+        PositionOffset = PositionOffset or Vector2.new(0,0)
+        Properties.Position = Vector2.new(ScreenPos.X + PositionOffset.X, ScreenPos.Y + PositionOffset.Y)
+        for i,v in pairs(Properties) do
+			pcall(Set, NewText, i, v)
+		end
+		return NewText
+    else
+        NewText:Remove()
+        return nil
+    end
 end
 
 return module
